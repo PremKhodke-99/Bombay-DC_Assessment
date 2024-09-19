20681d941ab42a48b6aa258181f52bd030b455f4
Prem <premkhodke.99@gmail.com>
2024-09-20 01:52:56 +0530

    JS event listener

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-function open() {}
+var menubtn = document.querySelector("#menu");
+var closebtn = document.querySelector("#close")
+var form = document.querySelector(".form");
+var contact = document.querySelector(".contact");
+var footer = document.querySelector(".mobile");
 
-open();
+menubtn.addEventListener("click", () => {
+    form.style.display = "block";
+    contact.style.display = "none";
+    footer.style.display = "none";
+})
+
+closebtn.addEventListener("click", () => {
+    form.style.display = "none"
+    contact.style.display = "block";
+    footer.style.display = "block";
+})
